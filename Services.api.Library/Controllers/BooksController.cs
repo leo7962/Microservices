885ee606e1d1fcb23168199a3e89229ed3e6409c
leo7962 <ingenieroleonardo@outlook.com>
2023-01-12 15:42:20 -0500

    Created a new entity with their controller and use of all CRUD methods.

---
+++ Services.api.Library/Controllers/BooksController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services.api.Library.Core.Entities;
+using Services.api.Library.Repository;
+
+namespace Services.api.Library.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BooksController : ControllerBase
+{
+    private readonly IMongoRepository<BookEntity> _bookRepository;
+
+    public BooksController(IMongoRepository<BookEntity> bookRepository)
+    {
+        _bookRepository = bookRepository;
+    }
+
+    [HttpPost]
+    public async Task Post(BookEntity book)
+    {
+        await _bookRepository.InsertDocument(book);
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<BookEntity>>> Get()
+    {
+        return Ok(await _bookRepository.GetAll());
+    }
+
+    [HttpPost("pagination")]
+    public async Task<ActionResult<PaginationEntity<BookEntity>>> PostPagination(
+        PaginationEntity<BookEntity> pagination)
+    {
+        var results = await _bookRepository.PaginationByFilter(pagination);
+        return Ok(results);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<BookEntity>> GetById(string id)
+    {
+        var book = await _bookRepository.GetById(id);
+        return Ok(book);
+    }
+}
---
+++ Services.api.Library/Core/Entities/BookEntity.cs
@@ -0,0 +1,13 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace Services.api.Library.Core.Entities;
+
+[BsonCollection("Book")]
+public class BookEntity : Document
+{
+    [BsonElement("title")] public string Title { get; set; }
+    [BsonElement("description")] public string Description { get; set; }
+    [BsonElement("price")] public int Price { get; set; }
+    [BsonElement("`publicationDate")] public DateTime? PublicationDate { get; set; }
+    [BsonElement("author")] public AuthorEntity Author { get; set; }
+}
